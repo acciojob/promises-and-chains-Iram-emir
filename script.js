b2f698bc8651f036561fb726afd9a83d5d2bdb8e
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-31 19:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+ let form = document.getElementById("myForm");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let age = document.getElementById("age");
+  let name = document.getElementById("name");
+
+  if (!age.value || !name.value) {
+    alert("Please enter valid details");
+    return;
+  }
+
+  const p = new Promise((resolve, reject) => {
+    if (age.value > 18) {
+      setTimeout(() => {
+        resolve(`Welcome, ${name.value}. You can vote.`);
+      }, 4000);
+    } else {
+      setTimeout(() => {
+        reject(`Oh sorry ${name.value}. You aren't old enough.`);
+      }, 4000);
+    }
+  });
+
+  p.then((data) => {
+    alert(data);
+  }).catch((error) => {
+    alert(error);
+  });
+});
